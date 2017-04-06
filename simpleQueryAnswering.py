ff792d9fd694ca3165754ee1707f874820e07baa
Ankit Verma <v.ankit001@gmail.com>
2017-04-06 13:39:09 -0500

    babi2 capability using information extraction, faster and more efficient

--- simpleQueryAnswering.py
+++ simpleQueryAnswering.py
@@ -29,9 +29,12 @@ yesnowords = ["can", "could", "would", "is", "does", "has", "was", "were", "had"
 commonwords = ["the", "a", "an", "is", "are", "were", "."]
 questionwords = ["who", "what", "where", "when", "why", "how", "whose", "which", "whom"]
 motion_verbs = ['move', 'travel', 'journeyed', 'went', 'goes']
+possess_verbs = ['has', 'got', 'find', 'found', 'get',  'grabbed', 'took']
+left_verbs = ['put down', 'dropped', 'discarded', 'left']
 
 motion_verbs = [porter.stem(w) for w in motion_verbs]
-
+possess_verbs = [porter.stem(w) for w in possess_verbs]
+left_verbs = [porter.stem(w) for w in left_verbs]
 # Take in a tokenized question and return the question type and body
 def processquestion(qwords, question):
     
@@ -58,6 +61,8 @@ def processquestion(qwords, question):
 
     annotation = annotator.getAnnotations(question, dep_parse=True)
     pos = annotation['pos']
+    ner = annotation['ner']
+
     # Determine question type
     if questionword in ["who", "whose", "whom"]:
         type = "S-PER"
@@ -80,8 +85,14 @@ def processquestion(qwords, question):
     if target[0] in yesnowords:
         target = target[1:]
     
+    targetType = []
+    for t in target:
+        for ntag in ner:
+            if t == ntag[0]:
+                targetType.append(ntag[1])
+
     # Return question data
-    return (type, questionword, target)
+    return (type, questionword, target, targetType)
 
 def getSubject(pos):
     ret = []
@@ -111,22 +122,39 @@ def processLine(line, article, question, answer):
         line = " ".join(line.split()[1:])
         article.append(line)
 
+        annotation = annotator.getAnnotations(line, dep_parse=True)
+        pos = annotation['pos']
+        ner = annotation['ner']
+        srl = annotation['srl']
+
+        srltag = [k for k, v in srl[0].iteritems()]
+        v = porter.stem(srl[0]["V"])
+
+        if similar(v, motion_verbs):
+            objects = [p[0] for p in pos if p[1] == 'NN' or p[1] == 'NNP']
+            location[objects[0]].append(objects[1])
+            for obj in list(has[objects[0]]):
+                location[obj] = [objects[1]]
+        elif similar(v, possess_verbs):
+            objects = [p[0] for p in pos if p[1] == 'NN' or p[1] == 'NNP']
+            has[objects[0]].add(objects[1])
+            invert_has[objects[1]] = objects[0]
+        elif similar(v, left_verbs):
+            objects = [p[0] for p in pos if p[1] == 'NN' or p[1] == 'NNP']
+            has[objects[0]].discard(objects[1])
+            invert_has[objects[1]] = ""
+
     return qflag
     
-    # article = BeautifulSoup(article).get_text()
-    # article = ''.join([i if ord(i) < 128 else ' ' for i in article])
-    # article = article.replace("\n", " . ")
-    # article = sent_detector.tokenize(article)
 
-    # print article
-    # # Process questions file
-    # questions = open(questionsfilename, 'r').read()
-    # questions = questions.decode('utf-8')
-    # questions = questions.splitlines()
+debug = 0
+location = collections.defaultdict(list)
+act = collections.defaultdict(list)
+has = collections.defaultdict(set)
+invert_has = collections.defaultdict(list)
 
 def main():
     # Get command line arguments
-    debug = 0
     articlefilename = sys.argv[1]
     if len(sys.argv) == 3:
         questionsfilename = sys.argv[2]
@@ -143,8 +171,8 @@ def main():
         qflag = processLine(line, article, questions, answers)
         if not qflag:
             continue
-
         question = questions[-1]
+        answer = []
 
         # Tokenize question
         print "Q:", question
@@ -152,123 +180,79 @@ def main():
         questionPOS = nltk.pos_tag(qwords)
 
         # Process question
-        (type, questionword, target) = processquestion(qwords, question)
-
+        (type, questionword, target, targetType) = processquestion(qwords, question)
         # Answer yes/no questions
         if type == "YESNO":
             yesno.answeryesno(article, qwords)
             continue
 
-        # Get sentence keywords
-        searchwords = set(target).difference(commonwords)
-        dict = collections.Counter()
-
-        answer = []
-        # Find most relevant sentences
-        for (i, sent) in enumerate(article):
-            sentwords = nltk.word_tokenize(sent)
-            wordmatches = set(filter(set(searchwords).__contains__, sentwords))
-            dict[sent] = len(wordmatches)
-            if dict[sent] == 0:
-                continue
-
-            annotation = annotator.getAnnotations(sent, dep_parse=True)
-            
-            pos = annotation['pos']
-            ner = annotation['ner']
-            srl = annotation['srl']
-
-            subject = getSubject(pos)
-            
-            if debug:
-                print subject, target
-                print ner, srl
-
-            if set(target) & set(subject):
-                for tag in ner:
-                    if type == tag[1]:
-                        answer.append(tag[0])
-
-                srltag = [k for k, v in srl[0].iteritems()]
-                v =  porter.stem(srl[0]["V"])
-                for i, stag in enumerate(srltag):
-                    if srl[0][stag] in subject and questionword == "where" and similar(v, motion_verbs):
-                        answer.append(srl[0][srltag[i+1]])
+        if questionword == "where":
+            if target[-1] in location:
+                answer = [location[target[-1]][-1]]
 
         if answer:
             exp, actual = answer[-1], answers[-1]
-            print answer[-1], "-->", answers[-1]
 
             if actual in exp:
                 right += 1
             else:
                 wrong += 1
 
+            print answer[-1], "-->", answers[-1], "Accuracy ", (float(right)*100) /(float(right) + float(wrong)) 
+
     print "Accuracy of the task ", (float(right)*100) /(float(right) + float(wrong))
 
 
+def findEntity(article, target, targetType, questionword, stop=None):
+    # Find most relevant sentences
+    answer, newtarget, newtargetType = [], [], targetType
+    targetChange = False
+    dict = collections.Counter()
+    for (i, sent) in enumerate(article):
+        sentwords = nltk.word_tokenize(sent)
+        wordmatches = set(filter(set(target).__contains__, sentwords))
+        
+        dict[sent] = len(wordmatches)
+        if dict[sent] == 0:
+            continue
 
-  
+        annotation = annotator.getAnnotations(sent, dep_parse=True)
+        
+        pos = annotation['pos']
+        ner = annotation['ner']
+        srl = annotation['srl']
+
+        subject = getSubject(pos)
+        
+        if debug:
+            print subject, target
+            print ner, srl
+
+        if set(target) & set(subject):
+            for tag in ner:
+                if type == tag[1]:
+                    answer.append(tag[0])
+
+
+            srltag = [k for k, v in srl[0].iteritems()]
+            v =  porter.stem(srl[0]["V"])
+            for i, stag in enumerate(srltag):
+                if srl[0][stag] in subject and questionword == "where" :
+
+                    if 'PER' in "".join(targetType) and similar(v, motion_verbs):
+                        answer.append(srl[0][srltag[i+1]])
+
+                    elif similar(v, possess_verbs) and 'PER' not in "".join(targetType):
+                        newtarget += [n[0] for n in ner if n[1] == 'S-PER']
+                        newtargetType = ['PER']
+                        targetChange = True
+
+
+    if targetChange:
+        answer = findEntity(article, newtarget[-1:], newtargetType, questionword, None)
+
+    return answer
+
 if __name__ == '__main__':
     main()
-
-
-    # Focus on 10 most relevant sentences
-    # for (sentence, matches) in dict.most_common(10):
-    #     parse = json.loads(corenlp.parse(sentence))
-    #     sentencePOS = nltk.pos_tag(nltk.word_tokenize(sentence))
-    #     print sentencePOS, parse["sentences"][0]["words"]
-
-    #     # Attempt to find matching substrings
-    #     searchstring = ' '.join(target)
-    #     if searchstring in sentence:
-    #         startidx = sentence.index(target[0])
-    #         endidx = sentence.index(target[-1])
-    #         answer = sentence[:startidx]
-    #         done = True
-    
-    #     # Check if solution is found
-    #     if done:
-    #         continue
-
-    #     # Check by question type
-    #     answer = ""
-    #     for worddata in parse["sentences"][0]["words"]:
-            
-    #         # Mentioned in the question
-    #         if worddata[0] in searchwords:
-    #             continue
-            
-    #         if type == "PERSON":
-    #             if worddata[1]["NamedEntityTag"] == "PERSON":
-    #                 answer = answer + " " + worddata[0]
-    #                 done = True
-    #             elif done:
-    #                 break
-
-    #         if type == "PLACE":
-    #             if worddata[1]["NamedEntityTag"] == "LOCATION":
-    #                 answer = answer + " " + worddata[0]
-    #                 done = True
-    #             elif done:
-    #                 break
-
-    #         if type == "QUANTITY":
-    #             if worddata[1]["NamedEntityTag"] == "NUMBER":
-    #                 answer = answer + " " + worddata[0]
-    #                 done = True
-    #             elif done:
-    #                 break
-
-    #         if type == "TIME":
-    #             if worddata[1]["NamedEntityTag"] == "NUMBER":
-    #                 answer = answer + " " + worddata[0]
-    #                 done = True
-    #             elif done:
-    #                 answer = answer + " " + worddata[0]
-    #                 break
-            
-    # if done:
-    #     print answer
-
     
